f1acb70461c3037b200eb475c1d691688bc3a454
HarshitChandrabose <47792483+HarshitChandrabose@users.noreply.github.com>
2019-05-28 22:42:40 +0530

    Game build sucessful

---
+++ Assets/TreeSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawn : MonoBehaviour
+{
+    public float spawnRate = .25f;
+
+    public GameObject TREEPrefab;
+
+    private float nextTimeToSpawn = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnRate = Random.Range(0.3f, .9f);
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(TREEPrefab, new Vector2(12,1), Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+
+
+}
---
+++ Assets/flow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flow : MonoBehaviour
+{
+    public float speed;
+    public Renderer bg;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bg.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0f);
+    }
+}
